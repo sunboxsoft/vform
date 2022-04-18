02b24ef16860aa9c9768e5f516f0ce978a55e6aa
sunboxsoft <sunboxsoft@126.com>
2022-04-18 11:11:51 +0800

    左侧自定义字段 贾 开始加子组件前

---
+++ src/extension/samples/extension-schema.js
@@ -0,0 +1,56 @@
+export const cardSchema = {
+    type: 'card',
+    category: 'container',
+    icon: 'card',
+    widgetList: [],
+    options: {
+        name: '',
+        label: 'card',
+        hidden: false,
+        folded: false,
+        showFold: true,
+        cardWidth: '100%',
+        shadow: 'never',
+        customClass: '',
+    }
+}
+
+export const alertSchema = {
+    type: 'alert',
+    icon: 'alert',
+    formItemFlag: false,
+    options: {
+        name: '',
+        title: 'Good things are comingQQQwww...',
+        type: 'info',
+        description: '',
+        closable: true,
+        closeText: '',
+        center: true,
+        showIcon: false,
+        effect: 'light',
+        hidden: false,
+        onClose: '',
+        customClass: '',
+    }
+}
+
+export const alertjiaSchema = {
+    type: 'alertjia',
+    icon: 'village',
+    formItemFlag: false,
+    options: {
+        name: '',
+        title: 'Good things are coming 贾国强 按图索骥...',
+        type: 'info',
+        description: '',
+        closable: true,
+        closeText: '',
+        center: true,
+        showIcon: false,
+        effect: 'light',
+        hidden: false,
+        onClose: '',
+        customClass: '',
+    }
+}
